91ab1133bd6ce511d465f3ab302208ad73e9adb8
Nicholas <nskins@umich.edu>
2025-01-02 18:53:44 -0500

    Configure sqlite3 database

--- flurm-api/src/app.module.ts
+++ flurm-api/src/app.module.ts
@@ -2,9 +2,17 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [UsersModule],
+  imports: [UsersModule,
+    TypeOrmModule.forRoot({
+      type: "sqlite",
+      database: "flurm-db",
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true // TODO: switch to false for production
+    })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
