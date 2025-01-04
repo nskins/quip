e7d2ee02230cda7952d54bb69b897f055d4f3767
Nicholas <nskins@umich.edu>
2025-01-03 19:42:18 -0500

    Generate auth module and service

--- flurm-api/src/app.module.ts
+++ flurm-api/src/app.module.ts
@@ -3,6 +3,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [UsersModule,
@@ -11,7 +12,8 @@ import { TypeOrmModule } from '@nestjs/typeorm';
       database: "flurm-db",
       entities: [__dirname + '/**/*.entity{.ts,.js}'],
       synchronize: true // TODO: switch to false for production
-    })
+    }),
+    AuthModule
   ],
   controllers: [AppController],
   providers: [AppService],
