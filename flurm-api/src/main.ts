cb3190d2585af314f1b4a8ebf35400b83280dc77
Nicholas <nskins@umich.edu>
2025-01-10 20:16:28 -0500

    Set CORS options to allow frontend to make API requests

--- flurm-api/src/main.ts
+++ flurm-api/src/main.ts
@@ -3,6 +3,18 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // TODO: adjust origin for dev/prod environments.
+  const options = {
+    "origin": "*",
+    "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
+    "preflightContinue": false,
+    "optionsSuccessStatus": 204,
+    "credentials": true
+  };
+
+  app.enableCors(options);
+
   await app.listen(process.env.PORT ?? 3001);
 }
 bootstrap();
