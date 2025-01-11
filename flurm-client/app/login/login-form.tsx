111edcaaae49328138a07d030b5015dae3ebdcab
Nicholas <nskins@umich.edu>
2025-01-10 20:17:19 -0500

    Rewrite form as client component in order to utilize localStorage

--- flurm-client/app/login/login-form.tsx
+++ flurm-client/app/login/login-form.tsx
@@ -1,25 +1,40 @@
+'use client'
+import { useState } from 'react'
+
 export default function LoginForm() {
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    const [error, setError] = useState('')
+
+    const handleSubmit = async (e: any) => {
+        e.preventDefault();
+
+        const data = await fetch('http://localhost:3001/users/login', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({ email: email, password: password })
+        })
+
+        const json = await data.json()
+
+        if (json && json.access_token) {
+            localStorage.setItem('access_token', json.access_token)
+            // TODO: redirect to user dashboard
+        }
+        else {
+            setError("Invalid credentials")
+        }
+    }
+
     return (
-        <form action={login}>
-            <input type="text" name="email" />
-            <input type="password" name="password" />
-            <button type="submit">Log In</button>
-        </form>
+        <>
+            <h1>Login</h1>
+            {error && <p>{error}</p>}
+            <form onSubmit={handleSubmit}>
+                <input type="text" name="email" onChange={(e) => setEmail(e.target.value)} />
+                <input type="password" name="password" onChange={(e) => setPassword(e.target.value)} />
+                <button type="submit">Log In</button>
+            </form>
+        </>
     )
-}
-
-async function login(formData: FormData) {
-    'use server'
-
-    const email = formData.get('email')
-    const password = formData.get('password')
-
-    const data = await fetch('http://localhost:3001/users/login', {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({ email: email, password: password })
-    })
-
-    const json = await data.json()
-    console.log(json)
 }
