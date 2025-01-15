0cedd012d75e32d002b6d079b6b821b42ebe5e9f
Nicholas <nskins@umich.edu>
2025-01-14 21:26:03 -0500

    Add channels controller, entity, module, service

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -5,11 +5,13 @@ import { UsersModule } from './users/users.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthModule } from './auth/auth.module';
 import { dataSourceOptions } from 'db/data-source';
+import { ChannelsModule } from './channels/channels.module';
 
 @Module({
   imports: [UsersModule,
     TypeOrmModule.forRoot(dataSourceOptions),
-    AuthModule
+    AuthModule,
+    ChannelsModule
   ],
   controllers: [AppController],
   providers: [AppService],
---
+++ api/src/channels/channel.entity.ts
@@ -0,0 +1,16 @@
+import { Entity, Column, PrimaryGeneratedColumn, CreateDateColumn, UpdateDateColumn } from 'typeorm';
+
+@Entity()
+export class User {
+    @PrimaryGeneratedColumn()
+    id : number
+
+    @Column({ unique: true })
+    name: string
+
+    @CreateDateColumn()
+    createdAt: Date
+
+    @UpdateDateColumn()
+    updatedAt: Date
+}
