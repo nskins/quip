5b9a40fbbe72c24a0bd9638ce8e87878828b355b
Nicholas <nskins@umich.edu>
2025-01-14 21:35:06 -0500

    Add constructor and getAll function to channels service

--- api/src/channels/channels.service.ts
+++ api/src/channels/channels.service.ts
@@ -1,4 +1,13 @@
 import { Injectable } from '@nestjs/common';
+import { Channel } from './channel.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class ChannelsService {}
+export class ChannelsService {
+    constructor(@InjectRepository(Channel) private ChannelRepository : Repository<Channel>) {}
+
+    async getAll(): Promise<Channel[]>{
+        return await this.ChannelRepository.find();
+    }
+}
