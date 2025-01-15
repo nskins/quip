bda4bb52cd94b3e16db5a5bd2b12a2b4b5744f98
Nicholas <nskins@umich.edu>
2025-01-14 21:38:19 -0500

    Add endpoint to retrieve all channels

--- api/src/channels/channels.controller.ts
+++ api/src/channels/channels.controller.ts
@@ -1,4 +1,17 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param, UseGuards } from '@nestjs/common';
+import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
+import { Channel } from './channel.entity';
+import { ChannelsService } from './channels.service';
 
 @Controller('channels')
-export class ChannelsController {}
+export class ChannelsController {
+    constructor(
+        private channelsService : ChannelsService
+    ) {}
+
+    @UseGuards(JwtAuthGuard)
+    @Get('')
+    async getAll(): Promise<Channel[]> {
+        return await this.channelsService.getAll();
+    }
+}
