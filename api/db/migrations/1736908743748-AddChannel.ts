f11dc4a0a703d4c8cd9045326ca261faba09b274
Nicholas <nskins@umich.edu>
2025-01-14 21:48:32 -0500

    Add seed data for channels

--- api/db/migrations/1736908743748-AddChannel.ts
+++ api/db/migrations/1736908743748-AddChannel.ts
@@ -5,6 +5,8 @@ export class AddChannel1736908743748 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`CREATE TABLE "channel" ("id" integer PRIMARY KEY AUTOINCREMENT NOT NULL, "name" varchar NOT NULL, "createdAt" datetime NOT NULL DEFAULT (datetime('now')), "updatedAt" datetime NOT NULL DEFAULT (datetime('now')), CONSTRAINT "UQ_800e6da7e4c30fbb0653ba7bb6c" UNIQUE ("name"))`);
+
+        await queryRunner.query(`INSERT INTO "channel" (name) VALUES ('books'), ('fashion'), ('games'), ('random'), ('sports')`);
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
