97d312e038acfd52983a852c036572a09c662e3e
Nicholas <nskins@umich.edu>
2025-01-29 20:56:00 -0500

    Improve styling on header component

--- client/app/channels/[name]/header.tsx
+++ client/app/channels/[name]/header.tsx
@@ -2,9 +2,10 @@ import LogoutButton from './logout-button'
 
 export default function Header() {
     return (
-        <div className="flex flex-row">
-            <h1>FlamChat</h1>
-            <LogoutButton />
+        <div className="flex flex-row bg-pink-100 pt-1 pb-1 text-black">
+            <h1 className="text-2xl ml-4 self-center">FlamChat</h1>
+            <span className="grow" />
+            <LogoutButton className="mr-4" />
         </div>
     )
 }
--- client/app/channels/[name]/logout-button.tsx
+++ client/app/channels/[name]/logout-button.tsx
@@ -1,10 +1,16 @@
 'use client'
 import { logout } from './logout'
 
-export default function LogoutButton() {
+export default function LogoutButton({
+    className
+} : {
+    className : string
+}) {
     return (
-        <>
-            <button onClick={logout}>Logout</button>
-        </>
+        <button 
+            className={`bg-pink-300 p-2 rounded-md ${className}`} 
+            onClick={logout}>
+                Logout
+        </button>
     )
 }
