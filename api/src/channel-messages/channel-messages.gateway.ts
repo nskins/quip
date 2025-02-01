72228a99a315d98cfece9b4f5bff3e4c93da9d6c
Nicholas <nskins@umich.edu>
2025-02-01 13:32:32 -0500

    Display new messages in realtime to user on channel page

--- api/src/channel-messages/channel-messages.gateway.ts
+++ api/src/channel-messages/channel-messages.gateway.ts
@@ -1,7 +1,13 @@
 import { MessageBody, WebSocketGateway, WebSocketServer } from "@nestjs/websockets";
 import { ChannelMessage } from "./channel-message.entity";
 
-@WebSocketGateway()
+@WebSocketGateway(3002, { cors: {
+    "origin": "*",
+    "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
+    "preflightContinue": false,
+    "optionsSuccessStatus": 204,
+    "credentials": true
+  }, transports: ['websocket', 'polling']})
 export class ChannelMessagesGateway {
     @WebSocketServer()
     server;
--- client/app/channels/[name]/page.tsx
+++ client/app/channels/[name]/page.tsx
@@ -1,38 +1,87 @@
+'use client'
 import ChannelNavbar from "./channel-navbar";
 import { getChannels } from "./getChannels";
-import { getChannelMessages } from "./getChannelMessages"
+import { GetChannelMessageDto, getChannelMessages } from "./getChannelMessages"
+import { socket } from '../../socket';
 import MessageList from "./message-list";
 import Header from "./header";
 import NewMessagePiece from "./new-message-piece";
+import { useEffect, useState } from "react";
+import { useParams } from "next/navigation";
+import { Channel } from "./channel";
 
-export default async function ChannelNamePage({
-    params
-}: {
-    params: Promise<{ name: string }>
-}) {
-    const activeChannelName = (await params).name
+export default function ChannelNamePage() {
+    const initialMessages : GetChannelMessageDto[] = []
+    const initialChannels : Channel[] = []
+    const initialActiveChannelId : number = 0
 
-    const channels = await getChannels()
-    const activeChannel = channels.find(c => c.name === activeChannelName)
+    const [isConnected, setIsConnected] = useState(socket.connected);
+    const [messages, setMessages] = useState(initialMessages);
+    const [channels, setChannels] = useState(initialChannels);
+    const [activeChannelId, setActiveChannelId] = useState(initialActiveChannelId)
 
-    // TODO: We should handle an unavailable channel better.
-    if (activeChannel === undefined)
-        throw "404 CHANNEL NOT FOUND"
+    const params = useParams<{ name : string }>();
 
-    const currentTime = new Date().toISOString()
-    const messages = await getChannelMessages({ channelId: activeChannel.id, timestamp: currentTime })
+    useEffect(() => {
+        function onConnect() {
+            setIsConnected(true);
+        }
+
+        function onDisconnect() {
+            setIsConnected(false);
+        }
+
+        function onMessageCreated(message : GetChannelMessageDto) {
+            setMessages([...messages, message]);
+        }
+
+        socket.on('connect', onConnect);
+        socket.on('disconnect', onDisconnect);
+        socket.on('message-created', onMessageCreated);
+
+        return () => {
+            socket.off('connect', onConnect);
+            socket.off('disconnect', onDisconnect);
+            socket.off('message-created', onMessageCreated);
+        };
+    }, [messages]);
+
+    useEffect(() => {
+        const getData = async () => {
+            const channels = await getChannels();
+            setChannels(channels);
+
+            const activeChannel = channels.find(c => c.name === params.name);
+            
+            // TODO: we should handle this error better.
+            if (activeChannel === undefined)
+                throw "404 CHANNEL NOT FOUND"
+
+            setActiveChannelId(activeChannel.id)
+
+            const currentTime = new Date().toISOString()
+
+            const messages = await getChannelMessages({
+                channelId: activeChannel.id, 
+                timestamp: currentTime 
+            })
+            setMessages(messages);
+        }
+
+        getData()
+    }, [])
 
     return (
         <div className="flex flex-col h-screen">
             <Header />
             <div className="flex flex-row w-screen grow">
-                <ChannelNavbar channels={channels} activeChannelId={activeChannel.id} />
+                <ChannelNavbar channels={channels} activeChannelId={activeChannelId} />
                 <div className="flex flex-col w-screen">
                     <div className="grow">
                         <MessageList messages={messages} />
                     </div>
                     <div>
-                        <NewMessagePiece channelId={activeChannel.id} />
+                        <NewMessagePiece channelId={activeChannelId} />
                     </div>
                 </div>
             </div>
