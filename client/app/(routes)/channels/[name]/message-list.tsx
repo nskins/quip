5ad542fb5617787fdd21dd8a1a4ce759094eb55c
Nicholas <nskins@umich.edu>
2025-02-04 20:19:00 -0500

    Add margins to messages to improve readability

--- client/app/(routes)/channels/[name]/message-list.tsx
+++ client/app/(routes)/channels/[name]/message-list.tsx
@@ -6,9 +6,9 @@ export default function MessageList({
     messages: GetChannelMessageDto[]
 }) {
     return (
-        <div className="flex flex-col gap-4 w-full">
+        <div className="flex flex-col w-full">
             {messages && messages.map(m =>
-                <div key={m.id}>
+                <div key={m.id} className="mx-4 my-2">
                     <div>{m.user.email}</div>
                     <div>{m.text}</div>
                     <div>{m.createdAt.toString()}</div>
