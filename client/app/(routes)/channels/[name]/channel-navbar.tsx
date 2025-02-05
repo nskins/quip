f4aaaf231978ca5a8a0e341d5f95c8f8835add84
Nicholas <nskins@umich.edu>
2025-02-04 21:36:40 -0500

    Improve UI in channel navbar

--- client/app/(routes)/channels/[name]/channel-navbar.tsx
+++ client/app/(routes)/channels/[name]/channel-navbar.tsx
@@ -9,12 +9,12 @@ export default function ChannelNavbar({
     activeChannelId: number 
 }) {
     return (
-        <div className="flex flex-col gap-4 w-48 bg-pink-500">
-            <h1>Channels</h1>
+        <div className="flex flex-col w-48 bg-pink-500">
+            <h1 className="p-2 font-bold text-xl">Channels</h1>
             {channels && channels.map(c =>
                 (c.id === activeChannelId) ? 
-                <Link href={`/channels/${c.name}`} className="bg-pink-200 text-black font-bold" key={c.id}>{c.name}</Link> : 
-                <Link href={`/channels/${c.name}`} key={c.id}>{c.name}</Link>         
+                <Link href={`/channels/${c.name}`} className="p-2 bg-pink-200 text-black font-bold" key={c.id}>{c.name}</Link> : 
+                <Link href={`/channels/${c.name}`} className="p-2 font-bold" key={c.id}>{c.name}</Link>         
             )}
         </div>
     )
