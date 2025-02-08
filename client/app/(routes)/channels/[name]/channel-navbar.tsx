471dee8134e688ed4b11718f6145160107b2df3e
Nicholas <nskins@umich.edu>
2025-02-07 22:14:08 -0500

    Simplify link rendering on channel navbar

--- client/app/(routes)/channels/[name]/channel-navbar.tsx
+++ client/app/(routes)/channels/[name]/channel-navbar.tsx
@@ -25,11 +25,20 @@ export default function ChannelNavbar({
     return (
         <div className="flex flex-col w-48 bg-pink-500">
             <h1 className="p-2 font-bold text-xl">Channels</h1>
-            {channels && channels.map(c =>
-                (c.id === activeChannelId) ? 
-                <Link href={`/channels/${c.name}`} className="p-2 bg-pink-200 text-black font-bold" key={c.id}>{c.name}</Link> : 
-                <Link href={`/channels/${c.name}`} className="p-2 font-bold" key={c.id}>{c.name}</Link>         
-            )}
+            {channels && channels.map(c => {
+                const style = (c.id === activeChannelId) ?
+                    "p-2 bg-pink-200 text-black font-bold" :
+                    "p-2 font-bold"
+                
+                    return (
+                        <Link 
+                            href={`/channels/${c.name}`} 
+                            className={style}
+                            key={c.id}>
+                                {c.name}
+                        </Link>
+                    );
+            })}
         </div>
     )
 }
