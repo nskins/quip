9c5ff8a389f596309673d68b028daba07d4f82d1
Nicholas <nskins@umich.edu>
2025-02-13 19:28:21 -0500

    Add additional default channels

--- api/db/migrations/1736908743748-AddChannel.ts
+++ api/db/migrations/1736908743748-AddChannel.ts
@@ -6,7 +6,23 @@ export class AddChannel1736908743748 implements MigrationInterface {
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`CREATE TABLE "channel" ("id" integer PRIMARY KEY AUTOINCREMENT NOT NULL, "name" varchar NOT NULL, "createdAt" datetime NOT NULL DEFAULT (datetime('now')), "updatedAt" datetime NOT NULL DEFAULT (datetime('now')), CONSTRAINT "UQ_800e6da7e4c30fbb0653ba7bb6c" UNIQUE ("name"))`);
 
-        await queryRunner.query(`INSERT INTO "channel" (name) VALUES ('books'), ('fashion'), ('games'), ('random'), ('sports')`);
+        await queryRunner.query(`INSERT INTO "channel" (name) VALUES
+            ('art'),
+            ('cooking'),
+            ('crafts'),
+            ('exercise'),
+            ('fashion'), 
+            ('games'),
+            ('languages'),
+            ('literature'),
+            ('music'),
+            ('philosophy'),
+            ('politics'),
+            ('random'),
+            ('religion'),
+            ('sports'),
+            ('television'),
+            ('writing')`);
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
