78c43116e1036d1627b008445deba79ea5fc4822
Nicholas <nskins@umich.edu>
2025-02-17 20:46:52 -0500

    Update login form with color, padding, etc.

--- client/app/(routes)/(auth)/login/login-form.tsx
+++ client/app/(routes)/(auth)/login/login-form.tsx
@@ -10,13 +10,13 @@ export default function LoginForm() {
     const [state, formAction] = useActionState(login, initialState);
 
     return (
-        <div className="flex flex-col gap-4 w-80">
+        <div className="flex flex-col gap-4 w-80 bg-pink-400 p-4 rounded-lg text-black">
             <h1>Login</h1>
             {state && state.error && <p>{state.error}</p>}
             <form action={formAction} className="flex flex-col gap-4">
-                <input type="email" name="email" required placeholder="Email" />
-                <input type="password" name="password" required placeholder="Password" />
-                <button type="submit">Log In</button>
+                <input className="p-1 rounded-md" type="email" name="email" required placeholder="Email" />
+                <input className="p-1 rounded-md" type="password" name="password" required placeholder="Password" />
+                <button className="p-1 bg-pink-200 rounded-lg" type="submit">Log In</button>
             </form>
         </div>
     )
