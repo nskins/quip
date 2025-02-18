b0c803fc166d79c795c73dab69d81f034d3cf001
Nicholas <nskins@umich.edu>
2025-02-17 21:39:34 -0500

    Implement login form w/ abstract auth form

--- client/app/(routes)/(auth)/login/login-form.tsx
+++ client/app/(routes)/(auth)/login/login-form.tsx
@@ -1,23 +1,30 @@
 'use client'
-import { useActionState } from 'react'
 import { login } from 'api/login'
+import Input from '../input';
+import AuthForm from '../auth-form';
 
 export default function LoginForm() {
-    const initialState = {
-        error: ''
-    };
-
-    const [state, formAction] = useActionState(login, initialState);
+    const inputs = (
+        <>
+            <Input
+                type="email"
+                name="email"
+                placeholder="Email"
+            />
+            <Input
+                type="password"
+                name="password"
+                placeholder="Password"
+            />
+        </>
+    );
 
     return (
-        <div className="flex flex-col gap-4 w-80 bg-pink-400 p-4 rounded-lg text-black">
-            <h1>Login</h1>
-            {state && state.error && <p>{state.error}</p>}
-            <form action={formAction} className="flex flex-col gap-4">
-                <input className="p-1 rounded-md" type="email" name="email" required placeholder="Email" />
-                <input className="p-1 rounded-md" type="password" name="password" required placeholder="Password" />
-                <button className="p-1 bg-pink-200 rounded-lg" type="submit">Log In</button>
-            </form>
-        </div>
+        <AuthForm
+            onSubmit={login}
+            title="Login"
+            inputs={inputs}
+            submitText="Log in"
+        />
     )
 }
